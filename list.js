c1aa9ea8e2d1a1394123f1596a15d0e487590307
noumecha spaker <noumechaivan@gmail.com>
2023-03-28 05:23:07 +0100

    upd

--- list.js
+++ list.js
@@ -4,13 +4,13 @@ export function getList(addresses) {
         <head>
             <title> Adress Book </title>
             <meta charset="utf-8"/>
-            <link rel="stylesheet" href="style.css"/> 
+            <link rel="stylesheet" href="style.css"/>
         </head>
             <body>
                 <h2 style="
-                    text-align:center; 
-                    display:block; 
-                    padding:10px; 
+                    text-align:center;
+                    display:block;
+                    padding:10px;
                     background-color:black;
                     color:white;
                     top:0;
@@ -18,29 +18,30 @@ export function getList(addresses) {
                 >
                     Address book
                 <h2>
-        </body>    
-            <table border='1'>    
-                <thead>    
-                    <tr>   
-                        <th>Id</th>   
+        </body>
+            <table border='1'>
+                <thead>
+                    <tr>
+                        <th>Id</th>
                         <th>Image</th>
-                        <th>First Name</th>   
+                        <th>First Name</th>
                         <th>Last Name</th>
                         <th>Street</th>
                         <th>City</th>
                         <th>Country</th>
                         <th>delete</th>
                         <th>edit</th>
-                    </tr>   
+                    </tr>
                 </thead>
                 <tbody>
                     ${addresses.map(createRow).join('')}
                 </tbody>
                 </tfoot>
-                    <th colspan="8">address book v1</th> 
-                </tfoot> 
-            </table>    
-        </body>    
+                    <th colspan="8">address book v1</th>
+                    <th colspan="1"><a href="/new" style="text-transform:capitalize;text-decoration:none;color:white;">new</a></th>
+                </tfoot>
+            </table>
+        </body>
     </html>`
 }
 
@@ -48,12 +49,12 @@ function createRow(address) {
     const img = address.file ? `<img src="${address.file}" height="30" width="30">` : '';
     return `<tr>
         <td>${address.id}</td>
-        <td>${img}</td> 
-        <td>${address.firstname}</td> 
-        <td>${address.lastname}</td> 
-        <td>${address.street}</td> 
-        <td>${address.city}</td> 
-        <td>${address.country}</td> 
+        <td>${img}</td>
+        <td>${address.firstname}</td>
+        <td>${address.lastname}</td>
+        <td>${address.street}</td>
+        <td>${address.city}</td>
+        <td>${address.country}</td>
         <td><a href="/delete/${address.id}">delete</a></td>
         <td><a href="/edit/${address.id}">edit</a></td>
     </tr>`;
